da4f63d0114143d80412b4bec5439dfcbdfcb12a
Michael Giardino <mikelj@gmail.com>
2013-04-10 16:11:35 -0400

    Created ns2 style trace from SUMO

---
+++ SUMO/mobility.tcl
@@ -0,0 +1,395 @@
+$ns_ at 0.0 "$node_(24) setdest 16.45 445.05 0.0"
+$ns_ at 0.0 "$node_(16) setdest -1.65 436.85 0.0"
+$ns_ at 0.0 "$node_(31) setdest 148.35 436.85 0.0"
+$ns_ at 0.0 "$node_(11) setdest 295.05 436.85 0.0"
+$ns_ at 0.0 "$node_(8) setdest 598.35 436.85 0.0"
+$ns_ at 0.0 "$node_(82) setdest 748.35 436.85 0.0"
+$ns_ at 0.0 "$node_(51) setdest 733.55005 304.95 0.0"
+$ns_ at 0.0 "$node_(88) setdest 16.45 148.35 0.0"
+$ns_ at 0.0 "$node_(75) setdest 8.25 13.15 0.0"
+$ns_ at 0.0 "$node_(101) setdest 4.95 13.15 0.0"
+$ns_ at 0.0 "$node_(74) setdest 308.25 13.15 0.0"
+$ns_ at 0.0 "$node_(5) setdest 451.65 13.15 0.0"
+$ns_ at 0.0 "$node_(70) setdest 751.65 13.15 0.0"
+$ns_ at 0.0 "$node_(72) setdest 733.55005 4.95 0.0"
+  $node_(24)   set X_ 16.450000762939453	# SUMO-ID: F02_0
+  $node_(24)   set Y_ 445.04998779296875 
+  $node_(24)   set Z_ 0.0 
+  $node_(16)   set X_ -1.649999976158142	# SUMO-ID: F01_0
+  $node_(16)   set Y_ 436.8500061035156 
+  $node_(16)   set Z_ 0.0 
+  $node_(31)   set X_ 148.35000610351562	# SUMO-ID: F06_0
+  $node_(31)   set Y_ 436.8500061035156 
+  $node_(31)   set Z_ 0.0 
+  $node_(11)   set X_ 295.04998779296875	# SUMO-ID: F07_0
+  $node_(11)   set Y_ 436.8500061035156 
+  $node_(11)   set Z_ 0.0 
+  $node_(8)   set X_ 598.3499755859375	# SUMO-ID: F09_0
+  $node_(8)   set Y_ 436.8500061035156 
+  $node_(8)   set Z_ 0.0 
+  $node_(82)   set X_ 748.3499755859375	# SUMO-ID: F10_0
+  $node_(82)   set Y_ 436.8500061035156 
+  $node_(82)   set Z_ 0.0 
+  $node_(51)   set X_ 733.550048828125	# SUMO-ID: F03_0
+  $node_(51)   set Y_ 304.95001220703125 
+  $node_(51)   set Z_ 0.0 
+  $node_(88)   set X_ 16.450000762939453	# SUMO-ID: F04_0
+  $node_(88)   set Y_ 148.35000610351562 
+  $node_(88)   set Z_ 0.0 
+  $node_(75)   set X_ 8.25	# SUMO-ID: F00_0
+  $node_(75)   set Y_ 13.149999618530273 
+  $node_(75)   set Z_ 0.0 
+  $node_(101)   set X_ 4.949999809265137	# SUMO-ID: F12_0
+  $node_(101)   set Y_ 13.149999618530273 
+  $node_(101)   set Z_ 0.0 
+  $node_(74)   set X_ 308.25	# SUMO-ID: F075_0
+  $node_(74)   set Y_ 13.149999618530273 
+  $node_(74)   set Z_ 0.0 
+  $node_(5)   set X_ 451.6499938964844	# SUMO-ID: F08_0
+  $node_(5)   set Y_ 13.149999618530273 
+  $node_(5)   set Z_ 0.0 
+  $node_(70)   set X_ 751.6500244140625	# SUMO-ID: F11_0
+  $node_(70)   set Y_ 13.149999618530273 
+  $node_(70)   set Z_ 0.0 
+  $node_(72)   set X_ 733.550048828125	# SUMO-ID: F05_0
+  $node_(72)   set Y_ 4.949999809265137 
+  $node_(72)   set Z_ 0.0 
+  $node_(49)   set X_ null	# SUMO-ID: F13_0
+  $node_(49)   set Y_ null 
+  $node_(49)   set Z_ 0.0 
+  $node_(34)   set X_ null	# SUMO-ID: F01_1
+  $node_(34)   set Y_ null 
+  $node_(34)   set Z_ 0.0 
+  $node_(4)   set X_ null	# SUMO-ID: F00_1
+  $node_(4)   set Y_ null 
+  $node_(4)   set Z_ 0.0 
+  $node_(7)   set X_ null	# SUMO-ID: F01_2
+  $node_(7)   set Y_ null 
+  $node_(7)   set Z_ 0.0 
+  $node_(113)   set X_ null	# SUMO-ID: F01_3
+  $node_(113)   set Y_ null 
+  $node_(113)   set Z_ 0.0 
+  $node_(110)   set X_ null	# SUMO-ID: F00_2
+  $node_(110)   set Y_ null 
+  $node_(110)   set Z_ 0.0 
+  $node_(97)   set X_ null	# SUMO-ID: F00_3
+  $node_(97)   set Y_ null 
+  $node_(97)   set Z_ 0.0 
+  $node_(57)   set X_ null	# SUMO-ID: F01_4
+  $node_(57)   set Y_ null 
+  $node_(57)   set Z_ 0.0 
+  $node_(73)   set X_ null	# SUMO-ID: F02_1
+  $node_(73)   set Y_ null 
+  $node_(73)   set Z_ 0.0 
+  $node_(122)   set X_ null	# SUMO-ID: F01_5
+  $node_(122)   set Y_ null 
+  $node_(122)   set Z_ 0.0 
+  $node_(2)   set X_ null	# SUMO-ID: F06_1
+  $node_(2)   set Y_ null 
+  $node_(2)   set Z_ 0.0 
+  $node_(125)   set X_ null	# SUMO-ID: F09_1
+  $node_(125)   set Y_ null 
+  $node_(125)   set Z_ 0.0 
+  $node_(15)   set X_ null	# SUMO-ID: F10_1
+  $node_(15)   set Y_ null 
+  $node_(15)   set Z_ 0.0 
+  $node_(105)   set X_ null	# SUMO-ID: F04_1
+  $node_(105)   set Y_ null 
+  $node_(105)   set Z_ 0.0 
+  $node_(47)   set X_ null	# SUMO-ID: F00_5
+  $node_(47)   set Y_ null 
+  $node_(47)   set Z_ 0.0 
+  $node_(21)   set X_ null	# SUMO-ID: F08_1
+  $node_(21)   set Y_ null 
+  $node_(21)   set Z_ 0.0 
+  $node_(112)   set X_ null	# SUMO-ID: F11_1
+  $node_(112)   set Y_ null 
+  $node_(112)   set Z_ 0.0 
+  $node_(41)   set X_ null	# SUMO-ID: F05_1
+  $node_(41)   set Y_ null 
+  $node_(41)   set Z_ 0.0 
+  $node_(59)   set X_ null	# SUMO-ID: F03_1
+  $node_(59)   set Y_ null 
+  $node_(59)   set Z_ 0.0 
+  $node_(58)   set X_ null	# SUMO-ID: rAmb_0
+  $node_(58)   set Y_ null 
+  $node_(58)   set Z_ 0.0 
+  $node_(77)   set X_ null	# SUMO-ID: F12_1
+  $node_(77)   set Y_ null 
+  $node_(77)   set Z_ 0.0 
+  $node_(89)   set X_ null	# SUMO-ID: F01_6
+  $node_(89)   set Y_ null 
+  $node_(89)   set Z_ 0.0 
+  $node_(18)   set X_ null	# SUMO-ID: F13_1
+  $node_(18)   set Y_ null 
+  $node_(18)   set Z_ 0.0 
+  $node_(118)   set X_ null	# SUMO-ID: F01_7
+  $node_(118)   set Y_ null 
+  $node_(118)   set Z_ 0.0 
+  $node_(32)   set X_ null	# SUMO-ID: F00_4
+  $node_(32)   set Y_ null 
+  $node_(32)   set Z_ 0.0 
+  $node_(53)   set X_ null	# SUMO-ID: F00_6
+  $node_(53)   set Y_ null 
+  $node_(53)   set Z_ 0.0 
+  $node_(90)   set X_ null	# SUMO-ID: F00_7
+  $node_(90)   set Y_ null 
+  $node_(90)   set Z_ 0.0 
+  $node_(39)   set X_ null	# SUMO-ID: F01_8
+  $node_(39)   set Y_ null 
+  $node_(39)   set Z_ 0.0 
+  $node_(85)   set X_ null	# SUMO-ID: F00_8
+  $node_(85)   set Y_ null 
+  $node_(85)   set Z_ 0.0 
+  $node_(25)   set X_ null	# SUMO-ID: F01_9
+  $node_(25)   set Y_ null 
+  $node_(25)   set Z_ 0.0 
+  $node_(38)   set X_ null	# SUMO-ID: F00_9
+  $node_(38)   set Y_ null 
+  $node_(38)   set Z_ 0.0 
+  $node_(17)   set X_ null	# SUMO-ID: F02_2
+  $node_(17)   set Y_ null 
+  $node_(17)   set Z_ 0.0 
+  $node_(1)   set X_ null	# SUMO-ID: F01_10
+  $node_(1)   set Y_ null 
+  $node_(1)   set Z_ 0.0 
+  $node_(94)   set X_ null	# SUMO-ID: F06_2
+  $node_(94)   set Y_ null 
+  $node_(94)   set Z_ 0.0 
+  $node_(28)   set X_ null	# SUMO-ID: F09_2
+  $node_(28)   set Y_ null 
+  $node_(28)   set Z_ 0.0 
+  $node_(71)   set X_ null	# SUMO-ID: F10_2
+  $node_(71)   set Y_ null 
+  $node_(71)   set Z_ 0.0 
+  $node_(0)   set X_ null	# SUMO-ID: F13_2
+  $node_(0)   set Y_ null 
+  $node_(0)   set Z_ 0.0 
+  $node_(95)   set X_ null	# SUMO-ID: F03_2
+  $node_(95)   set Y_ null 
+  $node_(95)   set Z_ 0.0 
+  $node_(76)   set X_ null	# SUMO-ID: F00_10
+  $node_(76)   set Y_ null 
+  $node_(76)   set Z_ 0.0 
+  $node_(37)   set X_ null	# SUMO-ID: F075_1
+  $node_(37)   set Y_ null 
+  $node_(37)   set Z_ 0.0 
+  $node_(20)   set X_ null	# SUMO-ID: F08_2
+  $node_(20)   set Y_ null 
+  $node_(20)   set Z_ 0.0 
+  $node_(36)   set X_ null	# SUMO-ID: F11_2
+  $node_(36)   set Y_ null 
+  $node_(36)   set Z_ 0.0 
+  $node_(81)   set X_ null	# SUMO-ID: F05_2
+  $node_(81)   set Y_ null 
+  $node_(81)   set Z_ 0.0 
+  $node_(123)   set X_ null	# SUMO-ID: F07_1
+  $node_(123)   set Y_ null 
+  $node_(123)   set Z_ 0.0 
+  $node_(3)   set X_ null	# SUMO-ID: F12_2
+  $node_(3)   set Y_ null 
+  $node_(3)   set Z_ 0.0 
+  $node_(102)   set X_ null	# SUMO-ID: F01_11
+  $node_(102)   set Y_ null 
+  $node_(102)   set Z_ 0.0 
+  $node_(67)   set X_ null	# SUMO-ID: F00_11
+  $node_(67)   set Y_ null 
+  $node_(67)   set Z_ 0.0 
+  $node_(61)   set X_ null	# SUMO-ID: F00_12
+  $node_(61)   set Y_ null 
+  $node_(61)   set Z_ 0.0 
+  $node_(40)   set X_ null	# SUMO-ID: F01_12
+  $node_(40)   set Y_ null 
+  $node_(40)   set Z_ 0.0 
+  $node_(92)   set X_ null	# SUMO-ID: F01_13
+  $node_(92)   set Y_ null 
+  $node_(92)   set Z_ 0.0 
+  $node_(30)   set X_ null	# SUMO-ID: F04_2
+  $node_(30)   set Y_ null 
+  $node_(30)   set Z_ 0.0 
+  $node_(26)   set X_ null	# SUMO-ID: F00_13
+  $node_(26)   set Y_ null 
+  $node_(26)   set Z_ 0.0 
+  $node_(107)   set X_ null	# SUMO-ID: F01_14
+  $node_(107)   set Y_ null 
+  $node_(107)   set Z_ 0.0 
+  $node_(87)   set X_ null	# SUMO-ID: F00_14
+  $node_(87)   set Y_ null 
+  $node_(87)   set Z_ 0.0 
+  $node_(111)   set X_ null	# SUMO-ID: F01_15
+  $node_(111)   set Y_ null 
+  $node_(111)   set Z_ 0.0 
+  $node_(35)   set X_ null	# SUMO-ID: F06_3
+  $node_(35)   set Y_ null 
+  $node_(35)   set Z_ 0.0 
+  $node_(60)   set X_ null	# SUMO-ID: F09_3
+  $node_(60)   set Y_ null 
+  $node_(60)   set Z_ 0.0 
+  $node_(6)   set X_ null	# SUMO-ID: F13_3
+  $node_(6)   set Y_ null 
+  $node_(6)   set Z_ 0.0 
+  $node_(115)   set X_ null	# SUMO-ID: F10_3
+  $node_(115)   set Y_ null 
+  $node_(115)   set Z_ 0.0 
+  $node_(78)   set X_ null	# SUMO-ID: F03_3
+  $node_(78)   set Y_ null 
+  $node_(78)   set Z_ 0.0 
+  $node_(56)   set X_ null	# SUMO-ID: F04_3
+  $node_(56)   set Y_ null 
+  $node_(56)   set Z_ 0.0 
+  $node_(104)   set X_ null	# SUMO-ID: F08_3
+  $node_(104)   set Y_ null 
+  $node_(104)   set Z_ 0.0 
+  $node_(120)   set X_ null	# SUMO-ID: F11_3
+  $node_(120)   set Y_ null 
+  $node_(120)   set Z_ 0.0 
+  $node_(43)   set X_ null	# SUMO-ID: F05_3
+  $node_(43)   set Y_ null 
+  $node_(43)   set Z_ 0.0 
+  $node_(46)   set X_ null	# SUMO-ID: F00_15
+  $node_(46)   set Y_ null 
+  $node_(46)   set Z_ 0.0 
+  $node_(14)   set X_ null	# SUMO-ID: F01_16
+  $node_(14)   set Y_ null 
+  $node_(14)   set Z_ 0.0 
+  $node_(106)   set X_ null	# SUMO-ID: F00_16
+  $node_(106)   set Y_ null 
+  $node_(106)   set Z_ 0.0 
+  $node_(44)   set X_ null	# SUMO-ID: F02_3
+  $node_(44)   set Y_ null 
+  $node_(44)   set Z_ 0.0 
+  $node_(84)   set X_ null	# SUMO-ID: F01_17
+  $node_(84)   set Y_ null 
+  $node_(84)   set Z_ 0.0 
+  $node_(121)   set X_ null	# SUMO-ID: F12_3
+  $node_(121)   set Y_ null 
+  $node_(121)   set Z_ 0.0 
+  $node_(19)   set X_ null	# SUMO-ID: F01_18
+  $node_(19)   set Y_ null 
+  $node_(19)   set Z_ 0.0 
+  $node_(64)   set X_ null	# SUMO-ID: F00_17
+  $node_(64)   set Y_ null 
+  $node_(64)   set Z_ 0.0 
+  $node_(66)   set X_ null	# SUMO-ID: F00_18
+  $node_(66)   set Y_ null 
+  $node_(66)   set Z_ 0.0 
+  $node_(13)   set X_ null	# SUMO-ID: F01_19
+  $node_(13)   set Y_ null 
+  $node_(13)   set Z_ 0.0 
+  $node_(45)   set X_ null	# SUMO-ID: F00_19
+  $node_(45)   set Y_ null 
+  $node_(45)   set Z_ 0.0 
+  $node_(22)   set X_ null	# SUMO-ID: F06_4
+  $node_(22)   set Y_ null 
+  $node_(22)   set Z_ 0.0 
+  $node_(83)   set X_ null	# SUMO-ID: F07_2
+  $node_(83)   set Y_ null 
+  $node_(83)   set Z_ 0.0 
+  $node_(68)   set X_ null	# SUMO-ID: F09_4
+  $node_(68)   set Y_ null 
+  $node_(68)   set Z_ 0.0 
+  $node_(62)   set X_ null	# SUMO-ID: F10_4
+  $node_(62)   set Y_ null 
+  $node_(62)   set Z_ 0.0 
+  $node_(65)   set X_ null	# SUMO-ID: F13_4
+  $node_(65)   set Y_ null 
+  $node_(65)   set Z_ 0.0 
+  $node_(126)   set X_ null	# SUMO-ID: F04_4
+  $node_(126)   set Y_ null 
+  $node_(126)   set Z_ 0.0 
+  $node_(48)   set X_ null	# SUMO-ID: F12_4
+  $node_(48)   set Y_ null 
+  $node_(48)   set Z_ 0.0 
+  $node_(10)   set X_ null	# SUMO-ID: F08_4
+  $node_(10)   set Y_ null 
+  $node_(10)   set Z_ 0.0 
+  $node_(69)   set X_ null	# SUMO-ID: F11_4
+  $node_(69)   set Y_ null 
+  $node_(69)   set Z_ 0.0 
+  $node_(42)   set X_ null	# SUMO-ID: F05_4
+  $node_(42)   set Y_ null 
+  $node_(42)   set Z_ 0.0 
+  $node_(114)   set X_ null	# SUMO-ID: F01_20
+  $node_(114)   set Y_ null 
+  $node_(114)   set Z_ 0.0 
+  $node_(103)   set X_ null	# SUMO-ID: F00_20
+  $node_(103)   set Y_ null 
+  $node_(103)   set Z_ 0.0 
+  $node_(86)   set X_ null	# SUMO-ID: F01_21
+  $node_(86)   set Y_ null 
+  $node_(86)   set Z_ 0.0 
+  $node_(108)   set X_ null	# SUMO-ID: F03_4
+  $node_(108)   set Y_ null 
+  $node_(108)   set Z_ 0.0 
+  $node_(33)   set X_ null	# SUMO-ID: F075_2
+  $node_(33)   set Y_ null 
+  $node_(33)   set Z_ 0.0 
+  $node_(29)   set X_ null	# SUMO-ID: F02_4
+  $node_(29)   set Y_ null 
+  $node_(29)   set Z_ 0.0 
+  $node_(109)   set X_ null	# SUMO-ID: F00_21
+  $node_(109)   set Y_ null 
+  $node_(109)   set Z_ 0.0 
+  $node_(54)   set X_ null	# SUMO-ID: F01_22
+  $node_(54)   set Y_ null 
+  $node_(54)   set Z_ 0.0 
+  $node_(117)   set X_ null	# SUMO-ID: F00_22
+  $node_(117)   set Y_ null 
+  $node_(117)   set Z_ 0.0 
+  $node_(50)   set X_ null	# SUMO-ID: F01_23
+  $node_(50)   set Y_ null 
+  $node_(50)   set Z_ 0.0 
+  $node_(99)   set X_ null	# SUMO-ID: F01_24
+  $node_(99)   set Y_ null 
+  $node_(99)   set Z_ 0.0 
+  $node_(124)   set X_ null	# SUMO-ID: F00_23
+  $node_(124)   set Y_ null 
+  $node_(124)   set Z_ 0.0 
+  $node_(9)   set X_ null	# SUMO-ID: F00_24
+  $node_(9)   set Y_ null 
+  $node_(9)   set Z_ 0.0 
+  $node_(116)   set X_ null	# SUMO-ID: F01_25
+  $node_(116)   set Y_ null 
+  $node_(116)   set Z_ 0.0 
+  $node_(63)   set X_ null	# SUMO-ID: F06_5
+  $node_(63)   set Y_ null 
+  $node_(63)   set Z_ 0.0 
+  $node_(96)   set X_ null	# SUMO-ID: F09_5
+  $node_(96)   set Y_ null 
+  $node_(96)   set Z_ 0.0 
+  $node_(23)   set X_ null	# SUMO-ID: F13_5
+  $node_(23)   set Y_ null 
+  $node_(23)   set Z_ 0.0 
+  $node_(55)   set X_ null	# SUMO-ID: F10_5
+  $node_(55)   set Y_ null 
+  $node_(55)   set Z_ 0.0 
+  $node_(27)   set X_ null	# SUMO-ID: F08_5
+  $node_(27)   set Y_ null 
+  $node_(27)   set Z_ 0.0 
+  $node_(80)   set X_ null	# SUMO-ID: F11_5
+  $node_(80)   set Y_ null 
+  $node_(80)   set Z_ 0.0 
+  $node_(52)   set X_ null	# SUMO-ID: F05_5
+  $node_(52)   set Y_ null 
+  $node_(52)   set Z_ 0.0 
+  $node_(12)   set X_ null	# SUMO-ID: F03_5
+  $node_(12)   set Y_ null 
+  $node_(12)   set Z_ 0.0 
+  $node_(93)   set X_ null	# SUMO-ID: F12_5
+  $node_(93)   set Y_ null 
+  $node_(93)   set Z_ 0.0 
+  $node_(79)   set X_ null	# SUMO-ID: F02_5
+  $node_(79)   set Y_ null 
+  $node_(79)   set Z_ 0.0 
+  $node_(119)   set X_ null	# SUMO-ID: F04_5
+  $node_(119)   set Y_ null 
+  $node_(119)   set Z_ 0.0 
+  $node_(98)   set X_ null	# SUMO-ID: F00_25
+  $node_(98)   set Y_ null 
+  $node_(98)   set Z_ 0.0 
+  $node_(91)   set X_ null	# SUMO-ID: F01_26
+  $node_(91)   set Y_ null 
+  $node_(91)   set Z_ 0.0 
+  $node_(100)   set X_ null	# SUMO-ID: F00_26
+  $node_(100)   set Y_ null 
+  $node_(100)   set Z_ 0.0 
