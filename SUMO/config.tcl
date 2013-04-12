e0ed27497dff659bb0c8bc76f21cc5c188676968
Michael Giardino <mikelj@gmail.com>
2013-04-12 17:21:17 -0400

    Updated traces

--- SUMO/config.tcl
+++ SUMO/config.tcl
@@ -1,5 +1,5 @@
 # set number of nodes
-set opt(nn) 127.0
+set opt(nn) 221.0
 
 # set activity file
 set opt(af) $opt(config-path)
@@ -11,7 +11,7 @@ append opt(mf) /mobility.tcl
 
 # set start/stop time
 set opt(start) 0.0
-set opt(stop) 54.0
+set opt(stop) 186.0
 
 # set floor size
 set opt(x) 759
