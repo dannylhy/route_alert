30a0676f6d483a811fe4157e7d3219f15a39b4fb
Michael Giardino <mikelj@gmail.com>
2013-04-17 14:33:27 -0400

    run03

--- SUMO/run03/gentrace.sh
+++ SUMO/run03/gentrace.sh
@@ -5,3 +5,5 @@ sumoD -c grid6_4.sumocfg
 
 java -jar /home/mikel/school/GT/sumo-0.16.0/tools/traceExporter/traceExporter.jar ns2 -n ../grid6_4.net.xml -t netstate6_4.xml -a activity.tcl -m mobility.tcl -c config.tcl -p 1 -b 0 -e 250
 
+grep Amb activity.tcl > README
+grep F04 grid6_4.flo.xml >> README
