7c6f213e1534936301e6b7dcc62b8b375100bd69
Michael Giardino <mikelj@gmail.com>
2013-04-17 14:08:40 -0400

    changed network

---
+++ SUMO/run04/gentrace.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+duarouterD -c grid6_4.duarcfg
+
+sumoD -c grid6_4.sumocfg
+
+java -jar /home/mikel/school/GT/sumo-0.16.0/tools/traceExporter/traceExporter.jar ns2 -n ../grid6_4.net.xml -t netstate6_4.xml -a activity.tcl -m mobility.tcl -c config.tcl -p 1 -b 0 -e 250
+
